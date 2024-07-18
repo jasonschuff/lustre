2afbaec96e6603824306afe90ee45de860203deb
jasonschuff <166432448+jasonschuff@users.noreply.github.com>
2024-07-18 15:07:42 -0500

    Updated repo

--- CycleCloud/Lustre.v1.04/specs/default/cluster-init/files/lfsrepo.sh
+++ CycleCloud/Lustre.v1.04/specs/default/cluster-init/files/lfsrepo.sh
@@ -4,19 +4,19 @@ lustre_version=${1-2.12.4}
 cat << EOF >/etc/yum.repos.d/LustrePack.repo
 [lustreserver]
 name=lustreserver
-baseurl=https://downloads.whamcloud.com/public/lustre/lustre-${lustre_version}/el7/patchless-ldiskfs-server/
+baseurl=https://downloads.whamcloud.com/public/lustre/lustre-${lustre_version}/el8/patchless-ldiskfs-server/
 enabled=1
 gpgcheck=0
 
 [e2fs]
 name=e2fs
-baseurl=https://downloads.whamcloud.com/public/e2fsprogs/latest/el7/
+baseurl=https://downloads.whamcloud.com/public/e2fsprogs/latest/el8/
 enabled=1
 gpgcheck=0
 
 [lustreclient]
 name=lustreclient
-baseurl=https://downloads.whamcloud.com/public/lustre/lustre-${lustre_version}/el7/client/
+baseurl=https://downloads.whamcloud.com/public/lustre/lustre-${lustre_version}/el8/client/
 enabled=1
 gpgcheck=0
 EOF
